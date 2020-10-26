3c5e6a7cd76721700640a86d2ca4b6efb5fe0cc9
Tprezioso <tommyprezioso@gmail.com>
2020-10-25 22:04:22 -0400

    Initial Commit

---
+++ QuickActionSwuiftUIBlog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickActionSwuiftUIBlog
+//
+//  Created by Thomas Prezioso on 10/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickActionSwuiftUIBlog/QuickActionSwuiftUIBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickActionSwuiftUIBlogApp.swift
+//  QuickActionSwuiftUIBlog
+//
+//  Created by Thomas Prezioso on 10/25/20.
+//
+
+import SwiftUI
+
+@main
+struct QuickActionSwuiftUIBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
