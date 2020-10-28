2ba35aed54189a524dd210b9cdaf297d05ee68ef
Tprezioso <tommyprezioso@gmail.com>
2020-10-28 19:00:20 -0400

    adding some synamic buttons

--- QuickActionSwuiftUIBlog/QuickActionSwuiftUIBlogApp.swift
+++ QuickActionSwuiftUIBlog/QuickActionSwuiftUIBlogApp.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 @main
 struct QuickActionSwuiftUIBlogApp: App {
+    @Environment(\.scenePhase) var lifeCycle
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: lifeCycle) { (newLifeCyclePhase) in
+            switch newLifeCyclePhase {
+            case .active :
+                print("App is active")
+            case .inactive:
+                print("App is inactive")
+            case .background:
+                print("App is going to the Background")
+                // This will add our dynamic quick actions when our app is sent to the background on our device
+                addQuickActions()
+            @unknown default:
+                print("default")
+            }
+        }
+    }
+    
+    func addQuickActions() {
+        UIApplication.shared.shortcutItems = [
+            UIApplicationShortcutItem(type: "Tweet", localizedTitle: "Tweet"),
+            UIApplicationShortcutItem(type: "Call", localizedTitle: "Call"),
+            UIApplicationShortcutItem(type: "Message", localizedTitle: "Message"),
+            UIApplicationShortcutItem(type: "Saved", localizedTitle: "Saved"),
+        ]
     }
 }
