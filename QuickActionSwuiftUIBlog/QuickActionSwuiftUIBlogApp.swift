b6a91f29385156e3d4f748904f8408e787a36dc6
Tprezioso <tommyprezioso@gmail.com>
2020-10-29 18:24:01 -0400

    added variable for passing data

--- QuickActionSwuiftUIBlog/QuickActionSwuiftUIBlogApp.swift
+++ QuickActionSwuiftUIBlog/QuickActionSwuiftUIBlogApp.swift
@@ -32,11 +32,24 @@ struct QuickActionSwuiftUIBlogApp: App {
     }
     
     func addQuickActions() {
+        var tweetuserInfo: [String: NSSecureCoding] {
+                    return ["name" : "tweet" as NSSecureCoding]
+                }
+                var calluserInfo: [String: NSSecureCoding] {
+                    return ["name" : "call" as NSSecureCoding]
+                }
+                var messageuserInfo: [String: NSSecureCoding] {
+                    return ["name" : "message" as NSSecureCoding]
+                }
+                var saveduserInfo: [String: NSSecureCoding] {
+                    return ["name" : "saved" as NSSecureCoding]
+                }
+        
         UIApplication.shared.shortcutItems = [
-            UIApplicationShortcutItem(type: "Tweet", localizedTitle: "Tweet"),
-            UIApplicationShortcutItem(type: "Call", localizedTitle: "Call"),
-            UIApplicationShortcutItem(type: "Message", localizedTitle: "Message"),
-            UIApplicationShortcutItem(type: "Saved", localizedTitle: "Saved"),
+            UIApplicationShortcutItem(type: "Tweet", localizedTitle: "Tweet", localizedSubtitle: "", icon: UIApplicationShortcutIcon(type: .compose)),
+            UIApplicationShortcutItem(type: "Call", localizedTitle: "Call", localizedSubtitle: "", icon: UIApplicationShortcutIcon(type: .audio)),
+            UIApplicationShortcutItem(type: "Message", localizedTitle: "Message", localizedSubtitle: "", icon: UIApplicationShortcutIcon(type: .message)),
+            UIApplicationShortcutItem(type: "Saved", localizedTitle: "Saved", localizedSubtitle: "", icon: UIApplicationShortcutIcon(type: .love)),
         ]
     }
 }
